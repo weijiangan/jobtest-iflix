da0276f65d4029d45e3cd97a36c60ed965c416a5
Wei Jian Gan <weijiangan@outlook.com>
2019-10-10 13:49:29 +0800

    init knexfile with sqlite db

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./output/dev.sqlite3"
+    }
+  },
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // }
+};
